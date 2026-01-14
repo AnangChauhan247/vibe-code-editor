58e9f0ede03ad8f73b7383aca609420cc5c86f29
Anang Chauhan <anangchauhan24@gmail.com>
2026-01-15 03:02:50 +0530

    02Databse

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import { Button } from "@/components/ui/button";
+import { db } from "@/lib/db";
 import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
   return(
-    <div className="flex flex-col items-center justify-center h-screen bg-color-100">
+    <div className="flex flex-col items-center justify-center h-screen bg-[#414141]">
     <Button>
       Get Started
     </Button>
